e0961ba5981e9c663564cdfb5622c0e6e2afb674
karuna kashyap <kashyapkaruna60@gmail.com>
2023-06-23 14:18:22 +0530

    add point-task component in mobile screen

--- src/Components/Mobile/Mobile.js
+++ src/Components/Mobile/Mobile.js
@@ -3,13 +3,17 @@ import Avatar from '../Mobile/Avatar/Avatar'
 import Footer from './Footer/Footer'
 import Todo from './Todo/Todo'
 import Approve from './ApproveTask/Approve'
+import Point_task from './PointTask/Point_task'
+import GlobalStyles from '../../app/Globalstyle'
 
 const Mobile = () => {
     return (
         <div>
+            <GlobalStyles />
             <Avatar />
             <Todo />
             <Approve />
+            <Point_task />
             <Footer />
 
         </div>
