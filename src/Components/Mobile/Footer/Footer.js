ad77678a5d1cd4bd50a1f2867b1e326d4697243e
karuna kashyap <kashyapkaruna60@gmail.com>
2023-07-01 23:42:47 +0530

    manage footer width and Icon inside the footer

--- src/Components/Mobile/Footer/Footer.js
+++ src/Components/Mobile/Footer/Footer.js
@@ -1,44 +1,57 @@
-import React, { useEffect } from 'react';
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Footer.module.css'
 
 const Footer = () => {
-  const[footerwidth,setFooterWidth]=useState(0)
-  useEffect(()=>{
-    function handleFooterSize(){
-      const screenWidth = window.innerWidth-40;
-      console.log(screenWidth)
+  const [footerwidth, setFooterWidth] = useState(0)
+  useEffect(() => {
+    const handleFooterSize = () => {
+      const screenWidth = window.innerWidth - 40;
       setFooterWidth(screenWidth)
     }
     handleFooterSize()
-    window.addEventListener('resize',handleFooterSize())
-  return () => {
-    window.removeEventListener('resize',handleFooterSize())
-  }
-  },[])
+    window.addEventListener('resize', handleFooterSize)
+    return () => {
+      window.removeEventListener('resize', handleFooterSize)
+    }
+  }, []);
   return (
     <div>
-      <div className={styles.footer} style={{width:footerwidth}}>
-        <div className={styles.my_task_icon}>
-          <img src='/my task.svg' width={30} height={30}></img>
-
+      <div
+        style={{
+          width: footerwidth, position: 'absolute',
+          height: '55px',
+          borderRadius: '0px 107.5px 107.5px 0px',
+          backgroundColor: '#34675C',
+          display: 'block',
+          display: 'inline-block',
+          bottom: '0px',
+          position: 'fixed'
+        }}>
+        <div className={styles.allFooterIcon}>
+          <div class="grid grid-cols-4 gap-10">
+            <div class="...">
+              <div className={styles.my_task_icon}>
+                <img src='/my task.svg' width={30} height={30}></img>
+              </div>
+            </div>
+            <div class="...">
+              <div className={styles.chat_icon}>
+                <img src="chat icon.svg" width={30} height={30}></img>
+              </div>
+            </div>
+            <div class="...">
+              <div className={styles.create_icon}>
+                <img src="create icon (1).svg" width={30} height={30}></img>
+              </div>
+            </div>
+            <div class="...">
+              <div className={styles.create_icon_background}></div>
+              <div className={styles.invite_icon}>
+                <img src="invite-icon.svg" width={30} height={30} ></img>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className={styles.chat_icon}>
-          <img src="chat icon.svg" width={30} height={30}></img>
-
-        </div>
-        <div className={styles.create_icon}>
-          <img src="create icon (1).svg" width={30} height={30}></img>
-
-        </div>
-        <div className={styles.create_icon_background}></div>
-        <div className={styles.invite_icon}>
-          <img src="invite-icon.svg" width={30} height={30} ></img>
-
-        </div>
-
-
-
       </div>
     </div>
   )
