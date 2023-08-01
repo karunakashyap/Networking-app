893186e708cccb32da439dfa2c3df4cbfaa5c721
karuna kashyap <kashyapkaruna60@gmail.com>
2023-08-01 10:59:09 +0530

    add some changes in otp screen

--- src/Components/Mobile/OTP/OTPLoader/OTPLoader.js
+++ src/Components/Mobile/OTP/OTPLoader/OTPLoader.js
@@ -3,7 +3,7 @@ import styles from './OTPLoader.module.css';
 import { useState, useEffect } from 'react';
 
 const OTPLoader = () => {
-  const [counter, setCounter] = useState(30);
+  const [counter, setCounter] = useState(10);
   useEffect(() => {
     counter > 0 && setTimeout(() => setCounter(counter - 1), 1000);
   }, [counter]);
@@ -12,12 +12,13 @@ const OTPLoader = () => {
       <div className={styles.loaderHeading}>
         <h1>you will receive a OTP within </h1>
       </div>
-      <div className={styles.loaderCode}>
-        <div className={styles.loaderCircle}></div>
+      <div className={styles.countdown}>
+        <div className={styles.countdownNumber}></div>
+        <svg height={400} width={400}>
+          <circle r="50" cx="55" cy="60"></circle>
+        </svg>
       </div>
-      <div >
-        <div>{counter === 0 ? (<div className={styles.resendBtn}><button>Resend</button></div>) : (<div className={styles.loaderTime}>{counter}</div>)}</div>
-      </div>
+      <div>{counter === 0 ? (<div className={styles.resendBtn}><button>Resend</button></div>) : (<div className={styles.loaderTime}>{counter}</div>)}</div>
     </div>
   )
 }
