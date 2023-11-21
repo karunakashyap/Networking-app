45b105a745fbef5444c48e12287347e3362d44c5
karuna kashyap <kashyapkaruna60@gmail.com>
2023-11-21 20:54:59 +0530

    add 404 page

---
+++ src/Components/Desktop/Page404/ContactUs/ContactUs.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './ContactUs.module.css'
+const ContactUs = () => {
+  return (
+    <div>
+      <div className={styles.contactButton}>
+        <button>Contact Us</button>
+      </div>
+    </div>
+  )
+}
+
+export default ContactUs
---
+++ src/Pages/Page404.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PageError from '../Components/Desktop/Page404/Page404';
+import GlobalStyles from '../app/Globalstyle';
+
+const Page404 = () => {
+  return (
+    <div>
+      <GlobalStyles />
+      <PageError />
+    </div>
+  )
+}
+
+export default Page404
