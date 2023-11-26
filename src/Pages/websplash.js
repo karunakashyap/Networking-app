797dc82ed122e5a0382d7f154f4db6a03f10a72c
karuna kashyap <kashyapkaruna60@gmail.com>
2023-11-26 21:45:41 +0530

    add web splash screen

---
+++ src/Pages/websplash.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import WebSplash from '../Components/Desktop/WebSplash/WebSplash'
+
+const websplash = () => {
+  return (
+    <div>
+        <WebSplash/>
+      
+    </div>
+  )
+}
+
+export default websplash
