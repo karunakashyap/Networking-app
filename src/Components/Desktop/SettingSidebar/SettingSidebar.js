b476bdb6412a0bed597e001fa7f551ec8963dfcf
karuna kashyap <kashyapkaruna60@gmail.com>
2023-11-30 15:18:11 +0530

    add logout popup in setting screen

--- src/Components/Desktop/SettingSidebar/SettingSidebar.js
+++ src/Components/Desktop/SettingSidebar/SettingSidebar.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import styles from './SettingSidebar.module.css'
-
+import React, { useState } from 'react';
+import styles from './SettingSidebar.module.css';
+import LogoutPopup from './LogoutPopup/LogoutPopup';
 const SettingSidebar = () => {
-  return (
-    <div>
-        <div className={styles.sidebarSetting}>
-          <div className={styles.settingHeading}>
-            <h1>Setting</h1>
-          </div>
-          <div className={styles.settingmenu }>
-            <ul>
-                <li><img src='/bell icon blue.svg'></img><p>Notification</p></li>
-                <li style={{position:'relative',right:'5px'}}><img  src='/privacy icons blue.svg'></img><p>Privacy</p></li>
-                <li><img src='/Vector blue.svg'></img><p>Help</p></li>
-                <li><img src='/about icon blue.svg'></img><p>About</p></li>
-                <li><img src='/logout-blue.svg'></img><p>Log out</p></li>
-            </ul>
-          </div>
+    const [popup, setPopup] = useState(false);
+    const [logoutImage, setLogoutImage] = useState('/logout-blue.svg');
+    const [logoutColor, setLogoutColor] = useState('');
+    const [opacity, setOpacity] = useState('1');
+    const [background, setBackground] = useState('#FFF')
+    const logoutPopup = () => {
+        setPopup(true);
+        setLogoutImage('/logout-yellow-icon.svg');
+        setLogoutColor('#C19B47')
+        setOpacity('0')
+        setBackground('#d9d9d99a')
+    }
+    const cancelButton = () => {
+        setOpacity('1')
+        setPopup(false)
+        setBackground('')
+        setLogoutColor('#2B8CA7')
+        setLogoutImage('/logout-blue.svg')
+        setBackground('#FFF')
+    }
+    return (
+        <div>
+
+            <div className={styles.sidebarSetting} style={{ opacity: opacity, backgroundColor: background }}>
+                {popup && <LogoutPopup cancelButton={cancelButton} />}
+                <div className={styles.settingHeading}>
+                    <h1>Setting</h1>
+                </div>
+                <div className={styles.settingmenu}>
+                    <ul>
+                        <li><img src='/bell icon blue.svg' width={22} height={22}></img><p>Notification</p></li>
+                        <li style={{ position: 'relative', right: '5px' }}><img src='/privacy icons blue.svg' width={28} height={28}></img><p>Privacy</p></li>
+                        <li><img src='/Vector blue.svg' width={22} height={22}></img><p>Help</p></li>
+                        <li style={{ position: 'relative', right: '6px' }}><img src='/about icon blue.svg' width={28} height={28}></img><p>About</p></li>
+                        <li style={{ position: 'relative', right: '6px' }} onClick={logoutPopup}><img src={logoutImage} width={26} height={26}></img><p style={{ color: logoutColor }}>Log out</p></li>
+                    </ul>
+                </div>
+            </div>
+
         </div>
-      
-    </div>
-  )
+    )
 }
 
 export default SettingSidebar
--- src/Components/Desktop/Sidebar/Sidebar.js
+++ src/Components/Desktop/Sidebar/Sidebar.js
@@ -3,78 +3,80 @@ import styles from './Sidebar.module.css'
 import Image from 'next/image';
 import SettingSidebar from '../SettingSidebar/SettingSidebar';
 const Sidebar = () => {
-    const [sidebarborderRadius,setsidebarborderRadius]=useState('0px 75.5px 75.5px 0px');
-    const [sidebarWidth,setsidebarWidth]=useState('320px');
-    const [sidebarText,setSidebarText]=useState(styles.sidebarText)
-    const [sidebarIcon,setSidebarIcon]=useState(styles.sidebarIcon);
-    const [settingSidebar,setsettingSidebar]=useState(false)
+    const [sidebarborderRadius, setsidebarborderRadius] = useState('0px 75.5px 75.5px 0px');
+    const [sidebarWidth, setsidebarWidth] = useState('320px');
+    const [sidebarText, setSidebarText] = useState(styles.sidebarText)
+    const [sidebarIcon, setSidebarIcon] = useState(styles.sidebarIcon);
+    const [settingSidebar, setsettingSidebar] = useState(false);
+    const [settingIcon, setSettingIcon] = useState('/settings-icon-gray.svg')
 
 
-    const showSetting=()=>{
+    const showSetting = () => {
         setsidebarWidth('100px')
         setsidebarborderRadius('0px 0px 0px 0px');
         setSidebarText(styles.sidebarTextHide)
         setSidebarIcon(styles.sidebarIconHide)
-        setsettingSidebar(true)
+        setsettingSidebar(true);
+        setSettingIcon('/setting.svg')
     }
     return (
         <div>
             <div >
-            <div className={styles.sidebar} style={{width:sidebarWidth,borderRadius:sidebarborderRadius}} >
-                <h1 className={styles.sidebarHeading}>
-                    <p className={sidebarText}>My Network</p>
-                    
-                </h1>
-                <div className={styles.allMenu}>
-                    <div className={styles.sidebarMenu}>
-                        <p className={sidebarText}>Notifications</p>
-                        <Image className={sidebarIcon} src='/bell-gray-icon.svg' width={20} height={20} alt='' ></Image>
-                    </div>
-                    <div className={styles.sidebarMenu}>
-                        <p className={sidebarText}>Create Task</p>
-                        <Image className={sidebarIcon} src='/plus-create-gray.svg' width={22} height={22} alt='' ></Image>
+                <div className={styles.sidebar} style={{ width: sidebarWidth, borderRadius: sidebarborderRadius }} >
+                    <h1 className={styles.sidebarHeading}>
+                        <p className={sidebarText}>My Network</p>
+
+                    </h1>
+                    <div className={styles.allMenu}>
+                        <div className={styles.sidebarMenu}>
+                            <p className={sidebarText}>Notifications</p>
+                            <Image className={sidebarIcon} src='/bell-gray-icon.svg' width={25} height={25} alt='' ></Image>
+                        </div>
+                        <div className={styles.sidebarMenu}>
+                            <p className={sidebarText}>Create Task</p>
+                            <Image className={sidebarIcon} src='/plus-create-gray.svg' width={28} height={28} alt='' ></Image>
+
+                        </div>
+                        <div className={styles.sidebarMenu}>
+                            <p className={sidebarText}>Chats</p>
+                            <Image className={sidebarIcon} src='/chat-gray-icon.svg' width={28} height={28} alt='' ></Image>
+
+                        </div>
+                        <div className={styles.sidebarMenu}>
+                            <p className={sidebarText}>All Tasks</p>
+                            <Image className={sidebarIcon} src='/create-report-gray.svg' width={26} height={26} alt='' ></Image>
+
+                        </div>
+                        <div className={styles.sidebarMenu}>
+                            <p className={sidebarText}>Members</p>
+                            <Image className={sidebarIcon} src='/group-member-gray.svg' width={26} height={26} alt='' ></Image>
+
+                        </div>
+                        <div className={styles.sidebarMenu}>
+                            <p className={sidebarText}>Setting</p>
+                            <Image className={sidebarIcon} src={settingIcon} width={24} height={24} alt='' onClick={showSetting} ></Image>
+
+                        </div>
+                        <div className={styles.sidebarMenu}>
+                            <p className={sidebarText}>Search</p>
+                            <Image className={sidebarIcon} src='/search-icon-gray.svg' width={28} height={28} alt='' ></Image>
+
+                        </div>
+                        <div className={styles.sidebarMenu}>
+                            <p className={sidebarText}>Invite</p>
+
+                            <Image className={sidebarIcon} src='/invite-icon-gray.svg' width={28} height={28} alt=''></Image>
+                        </div>
+                        {settingSidebar && <SettingSidebar />}
+
+
+
 
                     </div>
-                    <div className={styles.sidebarMenu}>
-                        <p className={sidebarText}>Chats</p>
-                        <Image className={sidebarIcon} src='/chat-gray-icon.svg' width={22} height={22} alt='' ></Image>
-
-                    </div>
-                    <div className={styles.sidebarMenu}>
-                        <p className={sidebarText}>All Tasks</p>
-                        <Image className={sidebarIcon} src='/create-report-gray.svg' width={20} height={20} alt='' ></Image>
-
-                    </div>
-                    <div className={styles.sidebarMenu}>
-                        <p className={sidebarText}>Members</p>
-                        <Image className={sidebarIcon} src='/group-member-gray.svg' width={22} height={22} alt='' ></Image>
-
-                    </div>
-                    <div className={styles.sidebarMenu}>
-                        <p className={sidebarText}>Setting</p>
-                        <Image className={sidebarIcon} src='/settings-icon-gray.svg' width={20} height={20} alt='' onClick={showSetting} ></Image>
-
-                    </div>
-                    <div className={styles.sidebarMenu}>
-                        <p className={sidebarText}>Search</p>
-                        <Image className={sidebarIcon} src='/search-icon-gray.svg' width={24} height={24} alt='' ></Image>
-
-                    </div>
-                    <div className={styles.sidebarMenu}>
-                        <p className={sidebarText}>Invite</p>
-
-                        <Image className={sidebarIcon} src='/invite-icon-gray.svg' width={24} height={24}  alt=''></Image>
-                    </div>
-                    {settingSidebar && <SettingSidebar/>}
-
-
 
 
                 </div>
 
-
-            </div>
-                           
 
             </div>
 
